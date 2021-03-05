8f175ceee530e8538c18e13b631227075c57f9f1
Josh Hills <hillsj4@coventry.ac.uk>
2021-03-05 01:16:54 +0000

    API SSL

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const Koa = require('koa')
-
+const https = require('https')
 const app = new Koa()
 
 app.use(async ctx => {
     ctx.body = 'Hello World';
-  });
-  
-app.listen(4001);
+})
+
+const opts = {
+    key: "/etc/letsencrypt/live/starshipnews.net/privkey.pem",
+    cert: "/etc/letsencrypt/live/starshipnews.net/fullchain.pem",
+    requestCert: false,
+	rejectUnauthorized: false
+}
+
+const port = process.env.PORT || 4001
+
+https.createServer(opts, app.callback()).listen(port, () => {
+	console.log(`Server is listening on port ${port}`)
+})
 
